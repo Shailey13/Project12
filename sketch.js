b57c0411acf5e6523200a0784c43dfecbe51ad5d
Shailey13 <84600597+Shailey13@users.noreply.github.com>
2021-06-22 15:02:47 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var apple;
+var appleImg;
+var leaf;
+var leafImg;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("Apple.png");
+  leafImg = loadImage("leaf.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+
+function draw() {
+  background(0);
+  rabbit.x = World.mouseX;
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  spawnApples();
+  spawnLeaves();
+
+  drawSprites();
+
+ 
+}
+
+function spawnApples() {
+if (frameCount%80 == 0) {
+  apple = createSprite(random(50, 350), 40, 10, 10);
+  apple.addImage(appleImg);
+  apple.velocityY = 3;
+  apple.scale = 0.1;
+}
+}
+
+function spawnLeaves() {
+  if (frameCount%80 == 0) {
+    leaf = createSprite(random(50, 350), 40, 10, 10);
+    leaf.addImage(leafImg);
+    leaf.velocityY = 2;
+    
+  leaf.scale = 0.1;
+  }
+}
+
